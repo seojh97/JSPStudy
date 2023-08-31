2d46c877e274037ea7703f721b4a09a7b181f9ae
luke3151 <luke3151@naver.com>
2023-08-31 17:56:27 +0900

    2023-08-31 JSP 커밋

--- src/main/java/utils/JSFunction.java
+++ src/main/java/utils/JSFunction.java
@@ -1,5 +1,8 @@
 package utils;
 
+import java.io.PrintWriter;
+
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.jsp.JspWriter;
 
 public class JSFunction {
@@ -35,4 +38,48 @@ public class JSFunction {
 		}
 		catch(Exception e) {}
 	}
+	
+	public static void alertlocation(HttpServletResponse resp, String msg, String url) {
+		/*
+		Java클래스에서는 JSP의 내장객체를 즉시 사용할 수 없으므로 반드시 매개변수로 전달받아 사용해야한다.
+		여기서는 웹브라우저에 문자열을 출력하기 위해 out내장객체를 JspWriter 타입으로 받은 후 사용하고있다.
+		*/
+		try{
+			//컨텐츠 타입을 설정
+			resp.setContentType("text/html;charset=UTF-8");
+			//PrintWriter객체를 통해 스크립트를 서블릿에서 직접 출력한다.
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+						  + "<script>"
+						  + "	alert	('" + msg + "');"
+						  + "	location.href='" + url + "';"
+						  + "</script>";
+			//해당 문자열을 웹브라우저에 출력한다.
+			writer.print(script);
+		}
+		catch(Exception e) {}
+	}
+	
+	/*
+	상단 2개의 메서드는 JSP에서 사용하기 위해 정의하였다.
+	JSP에서 out내장객체를 전달하면 매개변수로 받아서 사용한다.
+	아래 2개는 서블릿(Servlet)에서 사용하기 위해 메서드 오버로딩을 통해 정의하였다.
+	서블릿이 직접 요청을 받으므로 response내장객체를 통해 화면출력 용도인 PrintWriter 객체를 생성하여 사용한다.
+	*/
+	
+	public static void alertBack(HttpServletResponse resp, String msg) {
+		try {
+			resp.setContentType("text/html;charset=UTF-8");
+			PrintWriter writer = resp.getWriter();
+			String script = ""
+						  + "<script>"
+						  + "	alert('" + msg + "');"
+						  + "	history.back();"
+						  + "</script>";
+			writer.println(script);
+		}
+		catch(Exception e) {}
+	}
+	
+	
 }
